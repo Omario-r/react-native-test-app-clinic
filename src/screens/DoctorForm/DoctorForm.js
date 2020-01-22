0e94f1133f4c77519fe8e8b617541074c74c9701
Avilov. R <aviloff@yandex.ru>
2020-01-22 22:06:14 +1000

    feat(): start doctor form

---
+++ src/screens/DoctorForm/DoctorForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { Formik } from 'formik';
+
+class DoctorForm extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text>Form</Text>
+      </ScrollView>
+    );
+  }
+}

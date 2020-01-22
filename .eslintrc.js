e2a9a4ece8721b7ef08bcbf95bdfd5cb28c0651d
Avilov. R <aviloff@yandex.ru>
2020-01-22 22:05:38 +1000

    build(): add eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: [
+    "react",
+    "react-native"
+  ],
+  rules: {
+    'prettier/prettier': 0,
+    'react-native/no-inline-styles': 0,
+  },
 };
